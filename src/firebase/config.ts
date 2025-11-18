23470cf3a6b211f0ce19ebeafcf815eae419c3cf
Lucas Lima <lucas.aistudio1@gmail.com>
2025-11-18 14:06:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-148161959-37b37",
+  "appId": "1:645889260244:web:50841bfa82f0bf6af31941",
+  "apiKey": "AIzaSyBIG2P0o0OfT11qXwImbErs_Yx8wV4KnkQ",
+  "authDomain": "studio-148161959-37b37.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "645889260244"
+};
